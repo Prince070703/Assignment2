8e6c510e37691bdeefc8e26a8c35e6b9f563b2c7
Prince070703 <pkabc1738@gmail.com>
2025-07-09 16:24:29 +0530

    Add files via upload

---
+++ module3a.py
@@ -0,0 +1,10 @@
+#enter the number
+a = int(input('Enter the number :'))
+
+# condition
+
+if (a%2==0):
+ print(a , 'is the even number')
+ 
+else:
+ print(a , 'is the odd number')
---
+++ module3b.py
@@ -0,0 +1,6 @@
+total = 0
+
+for i in range(1, 51):
+    total += i  # Adds each number to total
+
+print("The sum of integers from 1 to 50 is:", total)
